9c4ccde13548ae7ddc435320edd33122ef376b2e
MetanoKid <carloscdcskate@gmail.com>
2017-03-20 23:44:28 +0100

    Adds scraping

--- src/GDCVaultScraper.js
+++ src/GDCVaultScraper.js
@@ -2,18 +2,22 @@
 
 // configuration
 let configuration = {
-	"userDefined": {
-		"source": {
-			//"type": "url",
-			//"uri": "http://www.gdcvault.com/browse/gdc-17"
-			"type": "file",
-			"uri": "input/GDC Vault.html"
+	userDefined: {
+		source: {
+			//type: "url",
+			//uri:  "http://www.gdcvault.com/browse/gdc-17"
+			type: "file",
+			uri:  "input/GDC Vault.html"
 		},
-		"outputFile": "gdc_vault_links.txt"
+		outputFile: "gdc_vault_links.txt"
 	},
-	"internal": {
-		"baseURL": "http//www.gdcvault.com",
-		"outputFolder": "output/"
+	internal: {
+		baseURL: "http//www.gdcvault.com",
+		outputFolder: "output/",
+		membersOnlyRegexes: {
+			vault:     /^loginPopup\(\'(.+)\'\); return false;$/,
+			sponsored: /^sponsorRegPopup\(\'\d+\',\'(\d+)\',\'\d+\'\)$/
+		}
 	}
 };
 
@@ -23,17 +27,95 @@ let fs      = require("fs");
 let mkdirp  = require("mkdirp");
 let cheerio = require("cheerio");
 
+// extracts a members-only link from an "onclick" attribute
+let extractMembersOnlyLink = function(onclickCallback) {
+	let matches = undefined;
+
+	// Vault's members
+	matches = onclickCallback.match(configuration.internal.membersOnlyRegexes.vault);
+
+	if(matches !== null) {
+		return matches[1];
+	}
+
+	// Sponsored's members
+	matches = onclickCallback.match(configuration.internal.membersOnlyRegexes.sponsored);
+
+	if(matches !== null) {
+		return matches[1];
+	}
+
+	return undefined;
+};
+
 // process HTML data to obtain an object with all of the data we're interested in
 let processHTML = function(html) {
-	let $ = cheerio(html);
+	let $ = cheerio.load(html);
 	let data = {};
+
+	// special entry for links we can't complete
+	data.unlinked = {};
+	let addToUnlinked = function(mediaType, mediaTitle) {
+		// ensure we've got the media type entry
+		data.unlinked[mediaType] = (data.unlinked[mediaType] || Array());
+
+		// add unlinked title
+		data.unlinked[mediaType].push(mediaTitle);
+	};
+
+	// media entries in the DOM
+	var mediaEntries = $("ul.media_items li a.session_item");
+
+	// iterate over all entries
+	mediaEntries.each(function(index, entry) {
+		let $entry = $(entry);
+
+		let mediaType = $entry.find(".media_type_image").attr("class").split(" ")[1];
+		let mediaTitle = $entry.find(".conference_info > p > strong").text();
+
+		// attempt to find the link
+		let link = undefined;
+		let membersOnly = false;
+
+		if($entry.attr("onclick")) {
+			let membersOnlyLink = extractMembersOnlyLink($entry.attr("onclick"));
+
+			if(membersOnlyLink !== undefined) {
+				link = membersOnlyLink;
+				membersOnly = true;
+			} else {
+				addToUnlinked(mediaType, mediaTitle);
+
+				// continue looping
+				return;
+			}
+		} else {
+			link = $entry.attr("href");
+		}
+
+		if(link === undefined) {
+			addToUnlinked(mediaType, mediaTitle);
+
+			// continue looping
+			return;
+		}
+
+		// ensure we've got the media type entry
+		data[mediaType] = (data[mediaType] || {});
+
+		// add entry
+		data[mediaType][mediaTitle] = {
+			url: configuration.internal.baseURL + link,
+			membersOnly: membersOnly
+		}
+	});
 
 	return data;
 };
 
 // build a string to be written from the data we've processed
 let convertProcessedDataToString = function(data) {
-	return JSON.stringify(data);
+	return JSON.stringify(data, undefined, "\t");
 };
 
 // write the contents of the processed data into the configured file
